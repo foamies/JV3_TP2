17fa1f60662b81cc6b255161d2b723062338b068
foamies <maximilien.fontaine@hotmail.com>
2023-12-17 12:36:21 -0500

    Système de pointage

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,29 @@ using TMPro;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private GameController gameController;
+    [SerializeField] private LevelManager levelManager;
+
     [SerializeField] private TextMeshProUGUI timerText;
     [SerializeField] private TextMeshProUGUI pointsText;
+    [SerializeField] private TextMeshProUGUI keyYes;
+    [SerializeField] private TextMeshProUGUI keyNo;
+
 
     private void Update()
     {
         gameController.UpdateTimer(Time.deltaTime);
         UpdateUITimer();
         UpdateUIPoints();
+        UpdateUIKey();
     }
 
     private void UpdateUITimer()
     {
+        if (gameController.CurrentTime <= 0)
+        {
+            levelManager.LoadScene("LastScene");
+        }
+
         string minutes = Mathf.Floor(gameController.CurrentTime / 60).ToString("00");
         string seconds = Mathf.Floor(gameController.CurrentTime % 60).ToString("00");
 
@@ -31,11 +42,30 @@ public class PlayerController : MonoBehaviour
         pointsText.text = $"{points}";
     }
 
+    private void UpdateUIKey()
+    {
+        if (gameController.Key == true)
+        {
+            keyYes.gameObject.SetActive(true);
+            keyNo.gameObject.SetActive(false);
+        }
+        else
+        {
+            keyYes.gameObject.SetActive(false);
+            keyNo.gameObject.SetActive(true);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Interactible")
+        if (other.gameObject.CompareTag("Interactible"))
         {
             gameController.InspectObject();
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.CompareTag("Key"))
+        {
+            gameController.FindKey();
             Destroy(other.gameObject);
         }
     }
--- Assets/Scripts/UILastScene.cs
+++ Assets/Scripts/UILastScene.cs
@@ -2,16 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UILastScene : MonoBehaviour
 {
+    [SerializeField] private GameController gameController;
+
     [SerializeField] private Button btnRetour;
+    [SerializeField] private TextMeshProUGUI pointage;
 
     private LevelManager levelManager;
 
     void Start()
     {
         levelManager = LevelManager.instance;
+
+        pointage.text = (gameController.Points).ToString();
 
         btnRetour.onClick.AddListener(ReturnMainMenu);
     }
